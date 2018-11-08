4278a7d84fed2ab996f10960e9562dc0f970d564
Alexandra Mirtcheva <alex@solanalabs.io>
2018-11-08 10:16:42 -0800

    add nav components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
+// component decorator
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root", //unique name to reference component
+  templateUrl: "./app.component.html", //templating (can do inline html)
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'ng7';
+  title = "ng7";
 }

6d6eb3f5b20dc7e1ef8cc20f3eed59792fc56423
Alexandra Mirtcheva <alex@solanalabs.io>
2018-11-09 16:15:39 -0800

    create contact form and add validation

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  selector: "app-contact",
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.scss"]
 })
 export class ContactComponent implements OnInit {
+  messageForm: FormGroup;
+  submitted = false;
+  success = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.messageForm = this.formBuilder.group({
+      name: ["", Validators.required],
+      message: ["", Validators.required]
+    });
   }
 
+  onSubmit() {
+    this.submitted = true;
+    // connect to back end to submit data - (send email)
+    if (this.messageForm.invalid) {
+      return;
+    }
+
+    this.success = true;
+  }
 }
